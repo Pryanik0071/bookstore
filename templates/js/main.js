0b2ee4b725d57324fa1b6f6fde87d82be20ab085
Dmitry M <di.mesler.2010@yandex.ru>
2021-08-03 01:23:26 +0300

    Add: js + nav menu list
    Signed-off-by: Dmitry M <di.mesler.2010@yandex.ru>

---
+++ templates/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // Кнопка в шапке
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".nav-menu")
+      .classList.toggle("nav__mobile--visible");
+  });
+});
