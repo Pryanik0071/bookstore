1262fe33f5a386d76420462407bd55d19885ba9e
Dmitry M <di.mesler.2010@yandex.ru>
2021-08-04 02:49:32 +0300

    Add: modal window
    Signed-off-by: Dmitry M <di.mesler.2010@yandex.ru>

--- templates/js/main.js
+++ templates/js/main.js
@@ -6,4 +6,67 @@ $(document).ready(function () {
       .querySelector(".nav-menu")
       .classList.toggle("nav__mobile--visible");
   });
+
+  // Modal Window
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+
+  closeModalButton.on("click", closeModal);
+
+  $(document).keydown(function (e) {
+    var modalIsOpen = $(".modal__dialog").hasClass("modal__dialog--visible");
+    if (e.keyCode === 27 && modalIsOpen) {
+      closeModal(e);
+    }
+  });
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+  $('input[type="tel"]').mask("+7 (000) 000-00-00");
+
+  // Валидация
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        "phone-modal": {
+          required: true,
+          minlength: 18,
+        },
+        phone: {
+          required: true,
+          minlength: 18,
+        },
+      },
+      messages: {
+        "name-modal": "Name is required",
+        name: "Name is required",
+        "phone-modal": {
+          required: "Phone is required",
+          minlength: jQuery.validator.format("Format input +7 (999) 999-99-99"),
+        },
+        phone: {
+          required: "Phone is required",
+          minlength: jQuery.validator.format("Format input +7 (999) 999-99-99"),
+        },
+        "email-news": "Email is required",
+        "email-modal": "Email is required",
+        email: "Email is required",
+      },
+    });
+  });
 });
