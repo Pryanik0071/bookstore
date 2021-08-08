792a6e6716641e98edd97db86d011e74cbad3b4e
Dmitry M <di.mesler.2010@yandex.ru>
2021-08-09 00:30:23 +0300

    Add: send mails php + thankyou.html
    Signed-off-by: Dmitry M <di.mesler.2010@yandex.ru>

---
+++ send.php
@@ -0,0 +1,67 @@
+<?php
+// Файлы phpmailer
+require 'templates/phpmailer/PHPMailer.php';
+require 'templates/phpmailer/SMTP.php';
+require 'templates/phpmailer/Exception.php';
+
+// в зависимости от пришедшей формы формируем сообщение:
+if(isset($_POST['email'])) {
+  $email = $_POST['email'];
+
+  $title = "Новая подписка на Ehya";
+  $body = "
+  <h2>Новая подписка</h2>
+  <b>Email:</b> $email
+  ";
+} else {
+  $name = $_POST['name-modal'];
+  $phone = $_POST['phone-modal'];
+  $email = $_POST['email-modal'];
+  $message = $_POST['message-modal'];
+  // Формирование самого письма
+  $title = "Новая подписка на Ehya";
+  $body = "
+  <h2>Новое обращение</h2>
+  <b>Имя:</b> $name<br>
+  <b>Телефон:</b> $phone<br>
+  <b>Email:</b> $email<br><br>
+  <b>Сообщение:</b><br>$message
+  ";
+}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = ''; // Логин на почте
+    $mail->Password   = ''; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('', ''); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('location: thankyou.html');
